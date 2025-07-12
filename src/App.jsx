ff4b5d0d67f14273239f7f08a3474969f5362fb8
Tushar Prajapati <149225920+Tushar123097@users.noreply.github.com>
2025-07-13 01:15:15 +0530

    Add files via upload
    Two hooks useState and UseEffect in Usestate 
    1: useState:
    It is a way for React to remember a value for you.
    Example: If you want to count how many times a button is clicked, useState helps you store and update that number.
    2: useEffect:
    It lets you run some code when something changes or when the page loads.
    Example: If you want to show a message every time the count changes, useEffect helps you do that.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import UseStatee from './components/Usestatee'
+import Useeffectt from './components/Useeffectt'
+import './App.css'
+
+function App() {
+  
+  return (
+  <div>
+   <UseStatee/>
+   <Useeffectt/>
+  </div>
+  )
+}
+
+export default App
